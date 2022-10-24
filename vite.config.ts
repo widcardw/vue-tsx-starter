8af812f0615c0137e3cd25aa916cac7eea04a002
widcardw <lui747353367@qq.com>
2022-10-24 10:42:33 +0800

    chore: fix css

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,22 @@ export default defineConfig({
       rehypePlugins: [
         [
           rehypePrettyCode,
-          { theme: { dark: 'vitesse-dark', light: 'vitesse-light' } },
+          {
+            theme: { dark: 'vitesse-dark', light: 'vitesse-light' },
+            onVisitLine(node) {
+              // Prevent lines from collapsing in `display: grid` mode, and
+              // allow empty lines to be copy/pasted
+              if (node.children.length === 0)
+                node.children = [{ type: 'text', value: ' ' }]
+            },
+            // Feel free to add classNames that suit your docs
+            onVisitHighlightedLine(node) {
+              node.properties.className.push('highlighted')
+            },
+            onVisitHighlightedWord(node) {
+              node.properties.className = ['highlighted']
+            },
+          },
         ],
       ], // <-- You can add other rehype plugins here
     }),
